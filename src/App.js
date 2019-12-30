37670ced1eeec7b19a4720a0ef85a609af6a30dd
Cristiano <cristiano.so@hotmail.com>
2019-12-30 18:29:55 -0300

    action addTodo and removeTodo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 
 import store from "./store";
 
+import TodoList from "./TodoList";
+
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <TodoList />
   </Provider>
 );
 
